174f536bfabe3e7171a3d02e3cd645815bd89249
Issblann <anaisabelpp@outlook.com>
2024-06-17 12:08:04 -0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { TextHeader } from './components/Text-header';
+
+function App() {
+  return (
+    <div>
+      <TextHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Text-header.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const TextHeader = () => {
+  return (
+    <div>
+      <h1>Texto de header top</h1>
+      <button>BUTTON</button>
+    </div>
+  );
+};
